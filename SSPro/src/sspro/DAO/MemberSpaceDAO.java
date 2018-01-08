344116dbb42782d4809b248bfb8b48821e260a6e
ssoogi <hjs2343@gmail.com>
2018-01-08 21:15:48 +0900

    login dao추가

--- SSPro/src/sspro/DAO/MemberSpaceDAO.java
+++ SSPro/src/sspro/DAO/MemberSpaceDAO.java
@@ -31,7 +31,57 @@ public class MemberSpaceDAO {
 		
 	}
 	
-	
+    public MemberSpaceVO select(String smember_id) {
+    	MemberSpaceVO memberspacevo = null;
+		int no = Integer.parseInt(smember_id);
+		
+		try {
+			memberspacevo = (MemberSpaceVO)sqlMap.queryForObject("memberspace.select", no);
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+    	return memberspacevo;
+	}
+    
+    
+	public ArrayList<MemberSpaceVO> selectAll() {
 
+        ArrayList<MemberSpaceVO> list=null;
+        
+			try {
+				list = (ArrayList<MemberSpaceVO>) sqlMap.queryForList("memberspace.selectAll");
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+			return list;
+			
+	}  //selectAll
+	
+ 
+	public boolean delete(String smember_id) {
+		 
+		
+		try {
+			int t = sqlMap.delete("memberspace.delete", smember_id);
+			if(t==1) return true;
+		} catch (SQLException e) {
+			
+			e.printStackTrace();
+		}
+		
+		return false;
+	}
+	
+	public boolean smLogin(String smember_email) {
+		int t=0;
+		
+		try {
+			t = (int) sqlMap.queryForObject("memberspace.login", smember_email);
+			if(t==1)return true;	
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return false;
+	}
 
 }
