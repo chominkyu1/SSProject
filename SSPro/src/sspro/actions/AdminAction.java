bcd58d8409e37633c48d80b6fff7eeebc9b55330
chominkyu1 <33860392+chominkyu1@users.noreply.github.com>
2018-01-09 15:59:24 +0900

    action

---
+++ SSPro/src/sspro/actions/AdminAction.java
@@ -0,0 +1,42 @@
+package sspro.actions;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.apache.struts.action.Action;
+import org.apache.struts.action.ActionForm;
+import org.apache.struts.action.ActionForward;
+import org.apache.struts.action.ActionMapping;
+
+import sspro.DAO.MemberSpaceDAO;
+import sspro.VO.MemberArtistVO;
+import sspro.VO.MemberSpaceVO;
+
+public class AdminAction extends Action{
+	@Override
+	public ActionForward execute(ActionMapping mapping, ActionForm form, HttpServletRequest request,
+			HttpServletResponse response) throws Exception {
+		
+		String action = request.getParameter("action");
+		String smember_email = request.getParameter("email");
+		String password = request.getParameter("password");
+		
+		MemberArtistVO memberartistvo = new MemberArtistVO();
+		MemberSpaceVO memberspacevo = new MemberSpaceVO();
+		ActionForward forword = null;
+		MemberSpaceDAO memberspacedao = new MemberSpaceDAO();
+		if(action.equals("login")) {
+		if(memberspacedao.smLogin(smember_email)) {
+			System.out.println("로그인");
+			forword = mapping.findForward("login");
+			
+		}else {
+			System.out.println("실패");
+			forword = mapping.findForward("fail");			
+		}
+		}
+		
+		
+		return forword;
+	}
+}
