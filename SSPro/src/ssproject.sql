ca86ede976bbf52d5e773206460e57e80ca50521
ssoogi <hjs2343@gmail.com>
2018-01-17 16:03:56 +0900

    sql table수정

--- SSPro/src/ssproject.sql
+++ SSPro/src/ssproject.sql
@@ -1,4 +1,4 @@
--- 테이블 생성 20180105 기준 최종
+-- 테이블 생성 20180117 기준 최종
 
 DROP TABLE ASK;
 
@@ -81,7 +81,10 @@ CREATE TABLE SPACEPOST
     SPACEPOST_PHONE    VARCHAR2(50) NOT NULL,
     SPACEPOST_ADDRESS    VARCHAR2(300) NOT NULL,
     SPACEPOST_SIZE    VARCHAR2(50) NOT NULL,
-    SPACEPOST_IMAGE    VARCHAR2(300) NOT NULL,
+    SPACEPOST_SECTION   VARCHAR2(50) NOT NULL,
+    SPACEPOST_IMAGE1    VARCHAR2(300) NOT NULL,
+    SPACEPOST_IMAGE2    VARCHAR2(300) NOT NULL,
+    SPACEPOST_IMAGE3    VARCHAR2(300) NOT NULL,
     SPACEPOST_STARTDATE    DATE NOT NULL,
     SPACEPOST_FINISHDATE    DATE NOT NULL,
     SPACEPOST_SORT    VARCHAR2(50) NOT NULL,
@@ -297,43 +300,44 @@ COMMIT;
 
 
 --MEMBER_SPACE DATA
-insert into MEMBER_Space values ('SM'||lpad (seq_SM.nextval,3,0) , '한정숙','HJS2343@NAVER.COM', '1234', '01042023831');
-insert into MEMBER_Space values ('SM'||lpad (seq_SM.nextval,3,0) , '조민규','mingyu@NAVER.COM', '1234', '01000000000');
+insert into MEMBER_Space values ('SM'||lpad (seq_SM.nextval,3,0) , '한정숙','hjs2343@naver.com', '1234', '01042023831');
+insert into MEMBER_Space values ('SM'||lpad (seq_SM.nextval,3,0) , '조민규','mingyu@naver.com', '1234', '01000000000');
 insert into MEMBER_Space values ('SM'||lpad (seq_SM.nextval,3,0) , '홍유진','ujin@daum.net', '1234', '01012345678');
-insert into MEMBER_Space values ('SM'||lpad (seq_SM.nextval,3,0) , '박민수','sooblend@NAVER.COM', '1234', '01057892345');
-insert into MEMBER_Space values ('SM'||lpad (seq_SM.nextval,3,0) , '정현지','hdubidup@NAVER.COM', '1234', '01054648787');
-insert into MEMBER_Space values ('SM'||lpad (seq_SM.nextval,3,0) , '전량원','won123@NAVER.COM', '1234', '01055555555');
+insert into MEMBER_Space values ('SM'||lpad (seq_SM.nextval,3,0) , '박민수','sooblend@naver.com', '1234', '01057892345');
+insert into MEMBER_Space values ('SM'||lpad (seq_SM.nextval,3,0) , '정현지','hdubidup@naver.com', '1234', '01054648787');
+insert into MEMBER_Space values ('SM'||lpad (seq_SM.nextval,3,0) , '전량원','won123@naver.com', '1234', '01055555555');
 
 --MEMBER_ARTIST DATA 
-insert into MEMBER_ARTIST values ('AM'||lpad (seq_AM.nextval,3,0) , '김영림','lim59@NAVER.COM', '1234', 'HONGIK', '01042022254','0','0');
-insert into MEMBER_ARTIST values ('AM'||lpad (seq_AM.nextval,3,0) , '한하늬','honey@NAVER.COM', '1234', '국민대학교', '01042022595','0','0');
-insert into MEMBER_ARTIST values ('AM'||lpad (seq_AM.nextval,3,0) , '한희성','sung89@NAVER.COM', '1234', 'HONGIK', '01078889454','0','0');
-insert into MEMBER_ARTIST values ('AM'||lpad (seq_AM.nextval,3,0) , '이길재','jealee@NAVER.COM', '1234', '이화여자대학교', '01089784451','0','0');
+insert into MEMBER_ARTIST values ('AM'||lpad (seq_AM.nextval,3,0) , '김영림','lim59@naver.com', '1234', 'HONGIK', '01042022254','0','0');
+insert into MEMBER_ARTIST values ('AM'||lpad (seq_AM.nextval,3,0) , '한하늬','honey@naver.com', '1234', '국민대학교', '01042022595','0','0');
+insert into MEMBER_ARTIST values ('AM'||lpad (seq_AM.nextval,3,0) , '한희성','sung89@naver.com', '1234', 'HONGIK', '01078889454','0','0');
+insert into MEMBER_ARTIST values ('AM'||lpad (seq_AM.nextval,3,0) , '이길재','jealee@naver.com', '1234', '이화여자대학교', '01089784451','0','0');
 insert into MEMBER_ARTIST values ('AM'||lpad (seq_AM.nextval,3,0) , 'sunny','sunny@daum.net', '1234', '서울대학교', '01089496151','0','0');
-insert into MEMBER_ARTIST values ('AM'||lpad (seq_AM.nextval,3,0) , '초미세','dust@NAVER.COM', '1234', '건국대학교', '01078945554','0','0');
+insert into MEMBER_ARTIST values ('AM'||lpad (seq_AM.nextval,3,0) , '초미세','dust@naver.com', '1234', '건국대학교', '01078945554','0','0');
 
 --SPACEPOST DATA
 
-insert into SPACEPOST values ('SP'||lpad (seq_SP.nextval,3,0), 'SM002','마포구','망원동 온실 망원도', '0504-0905-8898','서울특별시 마포구 망원동 395-36 4층','100*100','../src/image',to_date('18/02/01','yy/MM/DD'),
-to_date('18/09/01','yy/MM/DD'),'레스토랑','우드칩으로 채워진 야외 옥상정원의 특별함 야외테라스의 대형온실에서 만끽하는 하늘의 청량감 오묘한 색감의 네온조명 아래서 즐기는 라운지bar
+
+insert into SPACEPOST values ('SP'||lpad (seq_SP.nextval,3,0), 'SM002','마포구','망원동 온실 망원도', '0504-0905-8898','서울특별시 마포구 망원동 395-36 4층','100*100','2','../src/image','../src/image','../src/image',
+to_date('18/02/01','yy/MM/DD'),to_date('18/09/01','yy/MM/DD'),'레스토랑','우드칩으로 채워진 야외 옥상정원의 특별함 야외테라스의 대형온실에서 만끽하는 하늘의 청량감 오묘한 색감의 네온조명 아래서 즐기는 라운지bar
 DJING 이 가능한 전문장비 (CDJ2000 & DJM2000 NEXUS) 식물로 가득차있는 독특한 플랜테리어');
 
-insert into SPACEPOST values ('SP'||lpad (seq_SP.nextval,3,0), 'SM002','마포구','골든핸즈라운지', '050-7459-9549','서울특별시 마포구 망원동 439-44 1층 골든핸즈라운지','100*200','../src/image',to_date('18/02/01','yy/MM/DD'),
-to_date('18/09/01','yy/MM/DD'),'카페', '소규모 워크샵, 원데이 클라스와 같은 소규모형태의 모임과 잘 어울리는 공간입니다. 골든핸즈라운지의 따뜻하고 아늑한 분위기가 모임을 더욱 친근하고 편안하게 진행되도록 할 것입니다. * 
+insert into SPACEPOST values ('SP'||lpad (seq_SP.nextval,3,0), 'SM002','마포구','골든핸즈라운지', '050-7459-9549','서울특별시 마포구 망원동 439-44 1층 골든핸즈라운지','100*200','2','../src/image','../src/image','../src/image',
+to_date('18/02/01','yy/MM/DD'),to_date('18/09/01','yy/MM/DD'),'카페', '소규모 워크샵, 원데이 클라스와 같은 소규모형태의 모임과 잘 어울리는 공간입니다. 골든핸즈라운지의 따뜻하고 아늑한 분위기가 모임을 더욱 친근하고 편안하게 진행되도록 할 것입니다. * 
 원데이클래스/독서모임/북토크 등의 진행을 원하시는 분께 음료를 지원해드리고 있습니다. 원데이 클래스 진행 시 음료 3잔까지 무상제공해드리며, 수업의 성격을 고려해 협의 후 테이블 배치와 조닝 서비스를 해 드리고 있습니다. 
 원데이클래스 운영을 원하시는 분은 별도 연락바랍니다. ');
 
 
 insert into SPACEPOST values ('SP'||lpad (seq_SP.nextval,3,0),'SM004','강남구','조셉리미엄세미나룸', '02-123-1234', '도봉구 행복마을',
-'100*100','../src/image',to_date('18/02/01','yy/MM/DD'),to_date('18/09/01','yy/MM/DD'),
+'100*100','2','../src/image','../src/image','../src/image',to_date('18/02/01','yy/MM/DD'),to_date('18/09/01','yy/MM/DD'),
 '카페','양재천이 내려다 보이는 신축 건물내에 통 유리창으로 되어 있는 10평 규모의 대형 세미나룸 입니다  음료 무료 서비스가 제공 되며 워크샵등 다목적 모임과 중요한 미팅에 최적의 장소가 될것입니다.');
 
 insert into SPACEPOST values ('SP'||lpad (seq_SP.nextval,3,0),'SM005', '영등포구','공간다반사', '02-123-1234','서울특별시 영등포구 당산동6가 5 강남빌딩 4층 옥탑',
-'100*100','../src/image',to_date('18/02/01','yy/MM/DD'),to_date('18/09/01','yy/MM/DD'),'레스토랑',
+'100*100','2','../src/image','../src/image','../src/image',to_date('18/02/01','yy/MM/DD'),to_date('18/09/01','yy/MM/DD'),'레스토랑',
 '2호선, 9호선 당산역 도보 3분.나무바닥과 원목가구, 식물들로 따뜻한 분위기가 있는 공간입니다.셀프스튜디오 등으로 공간이 채워지고 있습니다.자세한 사진 및 운영은 블로그 / 인스타그램에서 만나실 수 있습니다.');
 
 insert into SPACEPOST values ('SP'||lpad (seq_SP.nextval,3,0),'SM005', '성동구','사진창고', '02-100-1234','서울특별시 성동구 성수동2가 309-59 사진창고','100*100',
-'../src/image',to_date('18/02/01','yy/MM/DD'),to_date('18/09/01','yy/MM/DD'),'카페','2015년 성수동에 오픈한 사진창고는 공장을 개조해 만든 인더스트리얼 느낌의 갤러리카페로 연중 사진전시가 진행되며 식사와 주류와 음료가 가능한 복합문화공간입니다. 수용 규모는 1층 홀은 45평 규모로 70~100명, 
+'2','../src/image','../src/image','../src/image',to_date('18/02/01','yy/MM/DD'),to_date('18/09/01','yy/MM/DD'),'카페','2015년 성수동에 오픈한 사진창고는 공장을 개조해 만든 인더스트리얼 느낌의 갤러리카페로 연중 사진전시가 진행되며 식사와 주류와 음료가 가능한 복합문화공간입니다. 수용 규모는 1층 홀은 45평 규모로 70~100명, 
 2층 강의실 및 세미나룸은 20명 수용이 가능합니다. 지금까지 다양한 영화와 드라마, CF 촬영이 이루어졌고 성수동의 핫플레이스로 자리매김하고 있습니다.');
  
 --HASH DATA
@@ -390,8 +394,8 @@ insert into WORK_REVIEW values ('RW'||lpad (seq_RW.nextval,3,0),5,'저희 카페와 �
 insert into WORK_REVIEW values ('RW'||lpad (seq_RW.nextval,3,0),4,'다음에도 전시하고 싶은 작품입니다','SM003','AM002');
 insert into WORK_REVIEW values ('RW'||lpad (seq_RW.nextval,3,0),1,'다음에는 받지 않으려구요 ','SM002','AM003');
 insert into WORK_REVIEW values ('RW'||lpad (seq_RW.nextval,3,0),5,'작가님 작품은 어디 걸어도 작품이 빛날 것 같습니다','SM003','AM003');
-insert into WORK_REVIEW values ('RW'||lpad (seq_RW.nextval,3,0),2,'','SM004','AM004');
-insert into WORK_REVIEW values ('RW'||lpad (seq_RW.nextval,3,0),4,'','SM005','AM005');
+insert into WORK_REVIEW values ('RW'||lpad (seq_RW.nextval,3,0),2,'제대로된 작품으로 준비하세요','SM004','AM004');
+insert into WORK_REVIEW values ('RW'||lpad (seq_RW.nextval,3,0),4,'네 나름 만족합니다','SM005','AM005');
 insert into WORK_REVIEW values ('RW'||lpad (seq_RW.nextval,3,0),5,'명작 부럽지 않은 분위기 입니다','SM006','AM006');
 
 
