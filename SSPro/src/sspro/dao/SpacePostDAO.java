42ed18bb82e79cff690f9dd05f73761b261fe6fc
ssoogi <hjs2343@gmail.com>
2018-01-17 18:49:06 +0900

    spacepostDAO에서 hashtag 불러오기

--- SSPro/src/sspro/dao/SpacePostDAO.java
+++ SSPro/src/sspro/dao/SpacePostDAO.java
@@ -37,9 +37,7 @@ public class SpacePostDAO {//한정숙: 공간게시글 기능 DAO
 		 return false;  
 	}  
 	
-	
-	
-	 public SpacePostVO select(String spacepost_id) {
+	 public SpacePostVO select(String spacepost_id) {//게시글 불러오기 
 		    //System.out.println(smember_id);
 	    	SpacePostVO spacepostvo = null;
 			try {
@@ -49,6 +47,16 @@ public class SpacePostDAO {//한정숙: 공간게시글 기능 DAO
 			}
 	    	return spacepostvo;
 		}
+	 
+	 public HashTagVO hashTagSelect(String spacepost_id) {//spacepost_id에 맞는 해시태그 불러오기 
+		 HashTagVO hashtagvo = null;
+		 try {
+			hashtagvo = (HashTagVO) sqlMap.queryForObject("spacepost.hashtagselect", spacepost_id);
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		 return hashtagvo;
+	 }
 	 
 	 public ArrayList<SpacePostVO> selectAll() {//공간게시글 전체불러오기
 
@@ -62,7 +70,7 @@ public class SpacePostDAO {//한정숙: 공간게시글 기능 DAO
 				return list;
 		}
 	 
-	 public boolean delete(String spacepost_id) {//
+	 public boolean delete(String spacepost_id) {//게시글 삭제
 			try {
 				int t = sqlMap.delete("spacepost.delete", spacepost_id);
 				if(t==1) return true;
