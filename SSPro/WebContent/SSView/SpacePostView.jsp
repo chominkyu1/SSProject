e02942b1d826608f4aafb871ed9a38840f977a1d
hyeonji jeong <hyeonji jeong@DESKTOP-7NKU5L7>
2018-01-26 03:32:47 +0900

    시작일 종료일 유효성

--- SSPro/WebContent/SSView/SpacePostView.jsp
+++ SSPro/WebContent/SSView/SpacePostView.jsp
@@ -163,7 +163,7 @@
             <p class="lead"><%= map.get("RSPACE_MEMO")%></p><br><%
            } %>
           </div> </div>
-          <form action="/SSPro/sectionshare/ask.do" method="post">
+          <form action="/SSPro/sectionshare/ask.do" method="post" id="form2">
         <input type="hidden" value="${memberartistvo.amember_name }" name="amember_name">
         <input type="hidden" value="${memberartistvo.amember_id }" name="amember_id">
         <input type="hidden" value="${spacepostvo.spacepost_id }"name="spacepost_id">
@@ -178,7 +178,7 @@
             <input type="date" id="Start" class="floatLabel" name="start" value="<?php echo date('Y-m-d'); ?>" required>
             <p>End Date: </p>
             <input type="date" id="End" class="floatLabel" name="end" value="<?php echo date('Y-m-d'); ?>"required >
-            <button type="submit">submit</button>
+            <button type="button" onclick="date()">submit</button>
           </div>
           <div class="thankyou"> <i class="fa fa-check-square-o"></i>
             <h1>Thank you!</h1>
@@ -190,7 +190,22 @@
       <div class="row"> </div>
     </div>
   </div>
-  
+  <div class="modal fade" id="modal">
+  <div class="modal-dialog">
+    <div class="modal-content">
+      <div class="modal-header">
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+        <h4 class="modal-title">확인해주세요!</h4>
+      </div>
+      <div class="modal-body">
+        <p>대여 시작일은 대여 종료일 보다 더 빨라야 합니다!</p>
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-info" data-dismiss="modal">확인</button>
+      </div>
+    </div><!-- /.modal-content -->
+  </div><!-- /.modal-dialog -->
+</div><!-- /.modal -->
 <footer id="footer">
   <div class="lower-footer">
     <div class="container-footer">
--- SSPro/WebContent/js/SpacePostView.js
+++ SSPro/WebContent/js/SpacePostView.js
@@ -1,23 +1,62 @@
- $(document).ready(function() {
-   $("#dtBox").DateTimePicker();
- });
+$(document).ready(function() {
+	$("#dtBox").DateTimePicker();
+});
 
- $("button").click(function() {
-   $(".reservations").addClass("close");
-   $(".thankyou").addClass("open");
- });
+$("button").click(function() {
+	$(".reservations").addClass("close");
+	$(".thankyou").addClass("open");
+});
 
- function updateTextInput(val) {
-   document.getElementById('textInput').value = val;
- }
+function updateTextInput(val) {
+	document.getElementById('textInput').value = val;
+}
+
+var didScroll;
+var lastScrollTop = 0;
+var delta = 5;
+var navbarHeight = $('header').outerHeight();
+$(window).scroll(function(event) {
+	didScroll = true;
+});
+setInterval(function() {
+	if (didScroll) {
+		hasScrolled();
+		didScroll = false;
+	}
+}, 250);
+
+function hasScrolled() {
+	var st = $(this).scrollTop();
+	if (Math.abs(lastScrollTop - st) <= delta)
+		return;
+	if (st > lastScrollTop && st > navbarHeight) {
+		$('header').removeClass('nav-down').addClass('nav-up');
+	} else {
+		if (st + $(window).height() < $(document).height()) {
+			$('header').removeClass('nav-up').addClass('nav-down');
+		}
+	}
+	lastScrollTop = st;
+}
 
 
-var didScroll; var lastScrollTop = 0; var delta = 5; var navbarHeight = $('header').outerHeight();
-$(window).scroll(function(event){ didScroll = true; });
-setInterval(function() { if (didScroll) { hasScrolled(); didScroll = false; } }, 250);
-
-function hasScrolled() { var st = $(this).scrollTop(); 
-if(Math.abs(lastScrollTop - st) <= delta) return;
-if (st > lastScrollTop && st > navbarHeight){
-$('header').removeClass('nav-down').addClass('nav-up'); } else {
-if(st + $(window).height() < $(document).height()) { $('header').removeClass('nav-up').addClass('nav-down'); } } lastScrollTop = st; }
+function date(){
+	  
+    var startDate = $('#Start').val();
+    var startDateArr = startDate.split('-');
+    
+    var endDate = $('#End').val();
+    var endDateArr = endDate.split('-');
+              
+    var startDateCompare = new Date(startDateArr[0], startDateArr[1], startDateArr[2]);
+    var endDateCompare = new Date(endDateArr[0], endDateArr[1], endDateArr[2]);
+      
+    if(startDateCompare.getTime() > endDateCompare.getTime()) {
+          
+    	$('#modal').modal();
+          
+        return;
+    }
+      
+    $("#form2").submit();
+};
